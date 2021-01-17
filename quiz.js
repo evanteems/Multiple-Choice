d530c17a2ff24fcc82288073582cd935741f1f93
Evan Teems <evanteems40@yahoo.com>
2021-01-17 02:28:06 -0800

    index.html

--- quiz.js
+++ quiz.js
@@ -4,7 +4,7 @@ const start = document.getElementById("start");
 
 const quiz = document.getElementById("Quiz");
 
-const questions = document.getElementById("questions");
+const question = document.getElementById("question");
 
 const Img = document.getElementById("img");
 
@@ -21,6 +21,8 @@ const timeGuage = document.getElementById("timeGuage");
 const progress = document.getElementById("theprogress");
 
 const scoreDiv = document.getElementById("score");
+
+let answerisWrong = 0
 
 // questions begin here!!
 let questions = [
@@ -70,6 +72,126 @@ let questions = [
         correct : "B"
 
     },{
-        question : "Which of the following is an example of an empty element"
+        question : "Which of the following is an example of an empty element?",
+
+        choiceA : "Correct!",
+
+        choiceB : "Wrong!",
+
+        ChoiceC : "Wrong!",
+
+        correct : "A"
+
+    },{
+        question : "What does JS stand for?",
+
+        choiceA : "Wrong!",
+
+        choiceB : "Wrong!",
+
+        choiceC : "Correct!",
+
+        correct : "C"
     }
-]
+];
+
+const lastQuestion = questions.length - 1;
+
+let runningQuestion = 0;
+
+let count = 0;
+
+const questionTime = 10;
+const guageWidth = 150;
+const guageUnit = guageWidth / questionTime;
+let TIMER;
+let score = 0
+
+// Rendering a Question
+
+function renderQuestion() {
+    let q = questions[runningQuestion];
+
+    question.innerHTML = "<p>"+ q.question +"<p>";
+
+    choiceA.innerHTML = q.choiceA;
+
+    choiceB.innerHTML = q.choiceB;
+
+    choiceC.innerHTML = q.choiceC;
+};
+
+start.addEventListener("click",startQuiz);
+// Starting the Quiz!
+function startQuiz() {
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+    renderProgress();
+    renderCounter();
+    TIMER = setInterval(renderCounter,1000);
+};
+
+// Progress
+
+function renderProgress() {
+    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
+        theprogress.innerHTML += "div class='prog' id="+ qIndex +"></div>";
+
+    }
+};
+
+function renderCounter() {
+    if(count <= questionTime) {
+        counter.innerHTML = count;
+        timeGuage.style.width = count * guageUnit + "px";
+        count++
+    }else{
+        count = 0;
+        anserisWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestion();
+        }else{
+            clearInterval(TIMER);
+            scoreRender();
+        };
+    };
+};
+
+// Check on Answers!!
+function checkAnswer(answer) {
+    if( answer == questions[runningQuestion].correct) {
+        score++;
+        answeriscorrect();
+    }else{
+        anserisWrong();
+    };
+    count = 0;
+    if(runningQuestion < lastQuestion) {
+        runningQuestion++;
+        renderQuestion();
+    }else{
+        clearInterval(TIMER);
+        scoreRender();
+    };
+};
+
+// When the answer is correct!
+function answeriscorrect() {
+    document.getElementById(runningQuestion);
+};
+
+// When the Answer is Incorrect!
+function answerisIncorrect() {
+    document.getElementById(runningQuestion);
+};
+
+// Rendering the Score
+function scoreRender() {
+    scoreDiv.style.display = "block";
+
+    const scorePerCent = Math.round(100 * score/questions.length);
+
+    scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
+};
